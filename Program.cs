d5f730f48709c4f88cde23d1431a049211602adc
nikossam7 <nikosamios159@gmail.com>
2023-12-01 13:11:11 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int guessingNumber;
+            int userInput;
+            bool guessTheNumber = true;
+
+            guessingNumber =  rnd.Next(1,100);
+
+            while (guessTheNumber)
+            {
+                Console.WriteLine("Guess the number: ");
+                userInput = Convert.ToInt32(Console.ReadLine());
+
+                if(userInput > guessingNumber)
+                {
+                    Console.WriteLine("Hide number is lower.");
+                }
+                else if(userInput < guessingNumber)
+                {
+                    Console.WriteLine("Hide number is higher.");
+                }            
+                else
+                {
+                    Console.WriteLine("Congratulation!You find it!");
+                    guessTheNumber = false;
+                }
+
+                
+
+            }
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GuessTheNumber")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GuessTheNumber")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("591527d3-640d-4a8b-9f03-8f38136c7152")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
